c76de50e1ee1b500ab1213e0542143438d440200
ThomasJones69 <tjones@ziggo.nl>
2017-10-30 13:06:45 +0100

    Bijgewerkt voorbeeld class and function

--- index.php
+++ index.php
@@ -15,19 +15,10 @@
     $boot2->diepgang=600;
     $boot3->diepgang=120;
     
-    
-    /* echo $boot1->bootnaam;
-    echo $boot1->bootlengte;
-    echo $boot1->diepgang;
-    
-    echo $boot2->bootnaam;
-    echo $boot2->bootlengte;
-    echo $boot2->diepgang;
-    echo $boot3->bootnaam;
-    echo $boot3->bootlengte;
-    echo $boot3->diepgang;
-    echo "<hr>";
-    */
+    $boot1->hoogte=3;
+    $boot2->hoogte=10;
+    $boot3->hoogte=12;
+        
     bootWeergeven($boot1);
     bootWeergeven($boot2);
     bootWeergeven($boot3);
@@ -38,16 +29,16 @@
     echo "De boot heeft lengte: ".$deBoot->bootlengte." meter";
     echo "<br>";
     echo "De boot heeft diepgang: ".$deBoot->diepgang." cm";
+    echo "<br>";
+    echo "De boot heeft hoogte: ".$deBoot->hoogte." meter";
     echo "<hr>";
     
 }
-
-    
-
     class Boot{
         public $bootnaam = "Nog niet bepaald";
         public $bootlengte = "Nog niet bepaald";
         public $diepgang = "Niet niet bepaald";
+        public $hoogte = "Nog niet bepaald";
         
     }
 
